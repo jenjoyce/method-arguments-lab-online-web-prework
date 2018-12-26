726ffb87d200dce220f3efd6120ae8d0491215cf
Jennifer Joyce <jenjoyce@mac.com>
2018-12-26 01:58:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
-#lib/introduction.rb
+#lib/introduction_with_language.rb
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction_with_language("name", "language")
+  puts "Hi, my name is #{name}" and I am learning to program in #{I}
 end
